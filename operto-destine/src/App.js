9682348c28473a69125b6318652d2d3db57f5cec
André Bruto <andrebruto@MacBook-Pro-de-Andre.local>
2020-12-15 17:04:07 -0800

    search form structure

--- operto-destine/src/App.js
+++ operto-destine/src/App.js
@@ -2,7 +2,35 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <div className="header">
+          <form className="header_form">
+            <input
+              className="header__destination-input"
+              type="search"
+              name="destinationInput"
+              placeholder="add your destination"
+            ></input>
+            <div>
+              <input
+                className="header__price-input"
+                type="number"
+                name="rangeMin"
+                placeholder="min"
+              ></input>
+              <input
+                className="header__price-input"
+                type="number"
+                name="rangeMax"
+                placeholder="max"
+              ></input>
+            </div>
+            <button className="header__btn">SEARCH</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
