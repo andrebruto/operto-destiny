a1746fd6ef44cc509986c78b3ed5485fc57d3ed3
André Bruto <andrebruto@MacBook-Pro-de-Andre.local>
2020-12-16 18:06:24 -0800

    fixed name and added navbar

---
+++ operto-destiny/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__container">
+        <h2 className="navbar__logo">destiny</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
